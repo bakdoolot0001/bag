d19d9c0928b501e4afab334397479d646625da8d
bakdoolot <nurisulajmanov.com>
2025-06-24 11:03:00 +0600

    second konf

--- src/components/pages/main/mainPages.jsx
+++ src/components/pages/main/mainPages.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Welcome from "./welcome/Welcome";
-<<<<<<< HEAD
 import Creative from "../creative/Creative";
 import Our from "../main/our/Our";
-=======
 import Our from "../our/our";
->>>>>>> 1918bb725b7533de37bde2d0ccd51e20af711623
 import Category from "./categories/Category";
 import Creative from "./creative/Creative";
 
